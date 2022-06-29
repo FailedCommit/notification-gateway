0c0f63b9c987d9825566f9b7f0aea400b801a585
cb-mohitkumar <mohit.kumar@chargeebee.com>
2022-06-29 16:49:24 +0530

    Initial commit

---
+++ src/main/java/com/cb/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.cb.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @GetMapping("/hi")
+    public String hello() {
+        return "Hi Mohit";
+    }
+}
